9f935ba293351de023f99a975742741a581c6cac
TranKienQuoc <trankienquoc12102004@gmail.com>
2026-05-03 22:10:51 +0700

    Update config

--- config/database.php
+++ config/database.php
@@ -1,10 +1,13 @@
 <?php
+// Set múi giờ cho PHP
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+
 class Database
 {
-    private $host = "localhost";
-    private $db_name = "quanlydulich";
-    private $username = "root";
-    private $password = "";
+    private $host = "sql100.infinityfree.com";
+    private $db_name = "if0_41728842_tour";
+    private $username = "if0_41728842";
+    private $password = "EoBN0fvjrfQSV"; // ⚠️ Nhớ đổi mật khẩu này sau nhé Quốc!
 
     public function connect()
     {
@@ -17,7 +20,13 @@ class Database
                 $this->password
             );
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // Set font chữ tiếng Việt
             $conn->exec("set names utf8mb4");
+
+            // Set múi giờ Việt Nam cho MySQL (Sửa $this->conn thành $conn)
+            $conn->exec("SET time_zone = '+07:00'");
+
         } catch (PDOException $e) {
             echo "Connection error: " . $e->getMessage();
         }
